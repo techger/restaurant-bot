d44b450c4f4af79bcadc1f5c3e9eae89cf8cda20
otsenguun <tsenguunkoko@gmail.com>
2016-05-12 13:46:22 -0700

    Revert "Food add acitivy өөрчлөв алдаатай !!"
    This reverts commit 11b801064d7e7b0bbcadd0306a356cbf976a53da.

--- app/src/main/java/com/menu/FoodAddActivity.java
+++ app/src/main/java/com/menu/FoodAddActivity.java
@@ -8,14 +8,9 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 
 import com.menu.Database.FoodAdapter;
 import com.menu.Database.MyDBHelper;
@@ -25,18 +20,16 @@ public class FoodAddActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1002;
 
-    AlertDialogManager alert = new AlertDialogManager();
     EditText name;
     EditText une;
-    Spinner turul;
+    EditText turul;
     EditText hemjee;
     Button save;
     FoodAdapter foodAdapter;
     Button pictureButton;
     ImageView picture;
-    Animation waveAnimation,shakeAnimation,myShakeAnimation;
     boolean hasBitmap = false;
-    ArrayAdapter<CharSequence> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,50 +41,34 @@ public class FoodAddActivity extends AppCompatActivity {
         foodAdapter = new FoodAdapter(this);
         name = (EditText)findViewById(R.id.foodNameEditText);
         une = (EditText)findViewById(R.id.uneEditText);
-        turul = (Spinner)findViewById(R.id.turulEditText);
-        adapter =ArrayAdapter.createFromResource(this,R.array.turul_names,android.R.layout.simple_expandable_list_item_1);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        turul.setAdapter(adapter);
+        turul = (EditText)findViewById(R.id.turulEditText);
         hemjee = (EditText)findViewById(R.id.hemjeeEditText);
         pictureButton = (Button) this.findViewById(R.id.addButtonSetPicture);
         picture = (ImageView) this.findViewById(R.id.addImage);
 
-        shakeAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
-
         pictureButton.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View v) {
-                                                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                                                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                                                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                                                 }
-                                             }
-                                         }
+             @Override
+             public void onClick(View v) {
+                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                     startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                 }
+             }
+         }
         );
         save = (Button)findViewById(R.id.addFoodButton);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!validate()) {
-                    return;
-                }
-
                 String nametext = name.getText().toString();
                 String unetext = une.getText().toString();
-                String turultext = turul.toString();
+                String turultext = turul.getText().toString();
                 String hemjeetext = hemjee.getText().toString();
 
 //                Cursor foods = myDBHelper.checkFood(nametext);
 
-                foodAdapter.addFood(new Food(nametext, unetext, turultext, hemjeetext, hemjeetext));
-                Log.d("===ADD===", "Амжилттай нэмлээ");
-                alert.showAlertDialog(FoodAddActivity.this, "    Амжилттай нэмэгдлээ ", "Хоол нэмэгдлээ...", true);
-                new android.os.Handler().postDelayed(
-                        new Runnable() {
-                            public void run() {
-                                finish();
-                            }
-                        }, 3000);
+                foodAdapter.addFood(new Food(nametext, unetext, turultext, hemjeetext , hemjeetext));
+                Log.d("===ADD===","Амжилттай нэмлээ");
 
 //                if (foods == null){
 //                    Log.d("====ADD===","Өгөгдлийн сангийн query алдаатай байна.");
@@ -110,47 +87,12 @@ public class FoodAddActivity extends AppCompatActivity {
         });
     }
 
-    public static boolean isNumericArray(String str) {
-        for (char c : str.toCharArray()) {
-            if (!Character.isDigit(c))
-                return false;
-        }
-        return true;
-    }
-
-    public boolean validate(){
-        boolean valid = true;
-        String nametext = name.getText().toString();
-        String unetext = une.getText().toString();
-        String turultext = turul.toString();
-        String hemjeetext = hemjee.getText().toString();
-
-        if(nametext.isEmpty()){
-            name.startAnimation(shakeAnimation);
-            name.setError("Хоосон байна !!");
-            valid = false;
-        }
-        if(unetext.isEmpty() &&  isNumericArray(unetext)==false){
-            une.startAnimation(shakeAnimation);
-            une.setError("Буруу утга оруулсан байна !!");
-            valid = false;
-        }
-        if(hemjeetext.isEmpty()&& isNumericArray(hemjeetext)==false){
-            hemjee.startAnimation(shakeAnimation);
-            hemjee.setError("Буруу утга оруулсан байна !!");
-            valid = false;
-        }
-
-
-        return valid;
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_food_add, menu);
         return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
